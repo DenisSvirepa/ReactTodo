50e4355b0fd6511fcd2bd2d84f6400e23ca89ba8
Свирепа Денис Николаевич <denisns@ipps.by>
2019-05-08 11:14:04 +0300

    RR

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
 import Todos from './components/Todos';
 import AddTodo from './components/AddTodo';
+import About from './components/pages/About';
 import Header from './components/layout/Header'
-
+import uuid from 'uuid';
 
 import './App.css';
 
@@ -10,17 +12,17 @@ class  App extends React.Component {
   state = {
     todos: [
       {
-        id: 1,
+        id: uuid.v4(),
         title: 'Take out the trash',
         completed: false
       },
            {
-        id: 2,
+        id: uuid.v4(),
         title: 'Dinner with wife',
         completed: true
       },
            {
-        id: 3,
+        id: uuid.v4(),
         title: 'Meeting with boss',
         completed: false
       },
@@ -40,18 +42,33 @@ markComplete = (id) =>  {
   this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)]})
  } 
 
+ addTodo = (title) => {
+   const newTodo = {
+     id: uuid.v4(),
+     title,
+     completed: false
+   }
+   this.setState({todos: [...this.state.todos, newTodo]})
+ }
+
   render(){
   return (
-  
-    <div className="App">
-     <div className='container'>
-    <Header />
-    <AddTodo />
-   <Todos todos={this.state.todos} markComplete={this.markComplete}
-      delTodo={this.delTodo}
-       />
-       </div>
-    </div>
+    <Router>
+      <div className="App">
+      <div className='container'>
+      <Header />
+      <Route exact path="/" render={props => (
+        <React.Fragment>
+          <AddTodo addTodo={this.addTodo}/>
+          <Todos todos={this.state.todos} markComplete={this.markComplete}
+        delTodo={this.delTodo}
+        />
+         </React.Fragment>   
+      )} />
+        <Route path="/about" component={About} />
+        </div>
+      </div>
+    </Router>  
   );
 }
 }
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 export default function Header() {
   return (
     <header style={headerStyle}>
       <h1>TodoList</h1>
+      <Link style={linkStyle} to="/">Home</Link> | <Link
+       style={linkStyle} to="/about">About </Link>
     </header>
   )
 
@@ -16,3 +19,8 @@ export default function Header() {
       textAlign: 'center',
       padding: '10px',
   }
+
+const linkStyle = {
+  color: '#fff',
+  textDecoration: 'none'
+}
